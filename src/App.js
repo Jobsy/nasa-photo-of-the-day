b4798bcad837245ebd62ab64f03ab317bbfdff65
Oluwajoba <oluwajobabello@gmail.com>
2019-09-11 23:13:20 +0100

    complete step 4 / break header into it own component / step 5

--- src/App.js
+++ src/App.js
@@ -1,58 +1,49 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from 'axios';
-import HomePage from "./components";
+import HomePage from "./component";
+import {Header} from "./components/header";
 import "./App.css";
 
 
 function App() {
   // debugger
 
-  
-  
+
+
 
 
   // function App() {
-    const [data, setData] = useState({ hits: [] });
-    // const [query, setQuery] = useState("react");
-  
-    useEffect(() => {
-      const fetchData = () => {
-        axios
-          .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
-          .then(res => {
-            setData(res.data);
+  const [data, setData] = useState({ hits: [] });
+  // const [query, setQuery] = useState("react");
+
+  useEffect(() => {
+    const fetchData = () => {
+      axios
+        .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+        .then(res => {
+          setData(res.data);
           console.log(res.data)
         }
-          );
-      };
-  
-      fetchData();
-    }, []);
-  // debugger
-    return (
-      <>
-        {/* <input value={query} onChange={e => setQuery(e.target.value)} /> */}
-        <ul>
-          {/* {data.hits.map(item => (
-            <li key={item.objectID}>
-              console.log({item})
-              <a href={item.url}>{item.title}</a>
-            </li>
-          ))} */}
-          
-            <h2>{data.title}</h2>
-            <img src={data.url}/>
-            <p>{data.explanation}</p>
-            <h4>{data.copyright}</h4>
-            <h3>{data.date}</h3>
-            
+        );
+    };
 
-          
-        </ul>
-      </>
-    );
+    fetchData();
+  }, []);
+  // debugger
+  return (
+    <>
+      <ul>
+        <Header headerAndTitle = {data.title}/>
+        {/* <h2></h2> */}
+        <img src={data.url} />
+        <p>{data.explanation}</p>
+        <h4>{data.copyright}</h4>
+        <h3>{data.date}</h3>
+      </ul>
+    </>
+  );
   // }
-  
+
   // return (
   //   <div className="App">
   //     <p>
---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Header = function (props) {
+
+    // console.log(props.headerAndTitle)
+    return (
+        <>
+            <h2>{props.headerAndTitle}</h2>
+        </>
+    );
+};
