6c5bb5e8c1454a44638391105bd132f083330953
Oluwajoba <oluwajobabello@gmail.com>
2019-09-12 18:31:35 +0100

    add a custom message for 404 error / stretch

--- src/App.js
+++ src/App.js
@@ -8,34 +8,54 @@ import "./App.css";
 //       Read through the instructions in the README.md file to build your NASA
 //       app! Have fun 🚀!
 
+// function App() {
+//   // Initialize state to hold the image URL
+//   const [dogPic, setDogPic] = useState("");
+//   useEffect(() => {
+//     // This axios GET request will return a single image
+//     axios
+//       .get("https://dog.ceo/api/breeds/image/random")
+//       // Which we then set to state
+//       .then(res => setDogPic(res.data.message))
+//       // Always include error handling
+//       .catch(err => console.log(err));
+//   }, []);
+
+//   return (
+//     <div className="App">
+//       <h1 className="App-header">We Love Puppers</h1>
+//       <img src={dogPic} alt="a random dog" />
+//       <input />
+//     </div>
+//   );
+// }
+
 function App() {
 
   const [data, setData] = useState({ hits: [] });
   const [query, setQuery] = useState("2019-09-11");
-
+  
   useEffect(() => {
     const fetchData = () => {
       axios
-        .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=" + query)
+        .get("https://api.nasa.gov/plantary/apod?api_key=DEMO_KEY&date=" + query)
         .then(res => {
           setData(res.data);
         }
-        );
+        )
+        .catch(error => {
+          setData(error.response.status)
+        })
     };
     fetchData();
   }, [query]);
 
-
-  // if (!data) {
-  //   return <h3>Loading...</h3>;
-  // }
-  // else {
   return (
     <>
       <input value={query} onChange={e => setQuery(e.target.value)} />
       <ul>
-        <h1>Astronomy Picture of the Day</h1>
-        <Header headerAndTitle={data.title} />
+        <h1 className="App-header">Astronomy Picture of the Day</h1>
+        <Header headerAndTitle={data.title} error={data} />
         <Body imgSrc={data.url} paragraph={data.explanation} />
         <Footer footerCopyright={data.copyright} footerDate={data.date} />
       </ul>
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,9 @@ import React from "react";
 export const Header = function (props) {
 
     if (!props.headerAndTitle) {
+        if (props.error === 404) {
+            return <h3>Sorry, can't fetch data. Check your address and resend</h3>;
+        }
         return <h3>Loading...</h3>;
     }
     else {
